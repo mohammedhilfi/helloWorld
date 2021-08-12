1b7451c6516b6bde254edc70282b32def3f8b7cc
mohammedhilfi <momohilfi03@Gmail.com>
2021-08-12 14:14:15 -0700

    we completed a var index

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('Hello World!');
+var firstName ='Momo';
+let lastName ='hilfi';
+let age = '17';
+
+firstName = "my dude";
+lastName = "potatos";
+age = '21';
+
+console.log('Hello'+ ' ' + firstName + ' ' + lastName + "!" + ' you are ' + age + ' years old');
